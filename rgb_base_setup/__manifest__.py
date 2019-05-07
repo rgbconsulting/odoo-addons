f44840765da452bebbd09fa5526395c9d46ba349
yfenes <yfenes.rgbconsulting@gmail.com>
2019-05-07 09:36:50 +0200

    [MIG] Migrated modules from v8

---
+++ rgb_base_setup/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 RGB Consulting SL
+		# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'RGB Base Setup',
+    'summary': 'Default base modules',
+    'category': 'RGB',
+    'website': "http://www.rgbconsulting.com",
+    'author': "RGB Consulting SL",
+    'license': 'AGPL-3',
+    'version': '12.0.1.0.0',
+    'depends': ['contacts',
+                'note',
+                'calendar',
+                'document',
+                # server-ux
+                'mass_editing',
+                'base_technical_features',
+                # server-auth
+                # 17-06-09 optional : 'password_security',
+                #server-brand
+                'disable_odoo_online',
+                # web
+                'web_dialog_size',
+                'web_searchbar_full_width',
+                'web_responsive',
+                #29-4-19 [NOT APROVED]'web_search_with_and',
+                # partner-contact
+                'base_location_geonames_import',
+    ],
+    'description': """
+RGB Base Setup
+==============
+
+* Installs the base modules for a basic setup.
+""",
+
+    "installable": True,
+
+}
---
+++ rgb_demo/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 RGB Consulting SL
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'RGB Demo',
+    'summary': 'Default addons for a basic demo erp',
+    'version': '12.0.1.0.0',
+    'category': 'RGB',
+    'website': "http://www.rgbconsulting.com",
+    'author': "RGB Consulting SL",
+    'license': 'AGPL-3',
+    'depends': ['rgb_base_setup',
+                'crm',
+                'sale',
+                'project',
+                'hr',
+                'stock',
+                'purchase',
+                'point_of_sale',
+                'mrp',
+                'fleet',
+                'hr_timesheet_sheet',
+                'hr_holidays',
+                'hr_expense'
+    ],
+    'description': """
+RGB Demo
+========
+* Installs the default modules for a basic demo erp.
+
+    """,
+
+    'demo': [
+        'views/demo_user.xml',
+    ],
+
+    'installable': True,
+
+}
