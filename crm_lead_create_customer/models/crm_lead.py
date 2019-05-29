f6fce5698be0c2a857c875579472f4d50e6533bb
yfenes <yfenes.rgbconsulting@gmail.com>
2019-05-29 12:12:32 +0200

    [IMP] Improved Partner assignation function

--- crm_lead_create_customer/models/crm_lead.py
+++ crm_lead_create_customer/models/crm_lead.py
@@ -13,9 +13,17 @@ class Lead(models.Model):
         for lead in self:
             context = self._context.copy()
             context.update({'active_model': 'crm.lead', 'active_id': lead.id})
-            partner_id = self.env['crm.partner.binding'].with_context(context)._find_matching_partner()
+            partner_id = lead.env['crm.partner.binding'].with_context(context)._find_matching_partner()
+            contact = None
+            if partner_id and lead.contact_name:
+                contact = self.env['res.partner'].search([('name', '=', lead.contact_name), ('parent_id', '=', partner_id)], limit=1)
+                if not contact:
+                    contact = lead._lead_create_contact(lead.contact_name, False, partner_id)
             if not partner_id:
                 lead.handle_partner_assignation('create', partner_id)
             else:
-                lead.handle_partner_assignation('exist', partner_id)
+                if lead.contact_name:
+                    lead.handle_partner_assignation('exist', contact.id)
+                else:
+                    lead.handle_partner_assignation('exist', partner_id)
         return True
