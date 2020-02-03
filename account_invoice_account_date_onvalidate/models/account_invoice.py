393d559c812f0bdbaeb51b75627fb7d16152d049
Jofre <jofre.lleo@rgbconsulting.com>
2020-02-03 13:28:37 +0100

    [ADD] New module (account_invoice_account_date_onvalidate)

---
+++ account_invoice_account_date_onvalidate/models/account_invoice.py
@@ -0,0 +1,15 @@
+# Copyright 2020 RGB Consulting
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def action_move_create(self):
+        """ Creates invoice related analytics and financial move lines """
+        self.filtered(lambda s: not s.date and s.type in ('in_invoice', 'in_refund')).write(
+            {'date': fields.Date.context_today(self)})
+        super(AccountInvoice, self).action_move_create()
