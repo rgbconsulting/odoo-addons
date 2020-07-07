cd6b788d1028f1f2aa7083544ac9d60e115a1f17
dmontull <david.montull@rgbconsulting.com>
2020-07-07 11:34:55 +0200

    [NEW] Add new modules

---
+++ rgb_demo/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 RGB Consulting SL
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'RGB Demo',
+    'summary': 'Default addons for a basic demo erp',
+    'version': '13.0.1.0.0',
+    'category': 'RGB',
+    'website': "https://www.rgbconsulting.com",
+    'author': "RGB Consulting SL",
+    'license': 'AGPL-3',
+    'depends': ['rgb_base_setup',
+                'crm',
+                'sale_management',
+                'project',
+                'hr',
+                'stock',
+                'purchase',
+                'point_of_sale',
+                'mrp',
+                'fleet',
+                'hr_timesheet_sheet',
+                'hr_holidays',
+                'hr_expense'
+    ],
+    'description': """
+RGB Demo
+========
+* Installs the default modules for a basic demo erp.
+
+    """,
+
+    'demo': [
+        'views/demo_user.xml',
+    ],
+
+    'installable': True,
+
+}
