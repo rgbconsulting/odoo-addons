9f17260f3e379e479b58b79c0ae4efb11944172a
Ramon <ramon.garcia@rgbconsulting.com>
2020-08-28 11:40:58 +0200

    [ADD] crm_utm_menus: new module added.

---
+++ crm_utm_menus/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 RGB Consulting
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'CRM UTM Menus',
+    'summary': 'CRM UTM Menus',
+    'version': '12.0.1.0.0',
+    'category': 'CRM',
+    'website': 'https://www.rgbconsulting.com',
+    'author': 'RGB Consulting',
+    'license': 'AGPL-3',
+    'depends': ['crm'],
+    'description': """
+CRM UTM Menus
+=============
+* This module adds marketing information UTMs in CRM configuration's menu.
+    """,
+
+    'data': [
+        'views/crm_views.xml',
+    ],
+}
