1447b93b2a86248271c36c4698dc36b2a189417e
Aleix <aleix.capdevila@rgbconsulting.com>
2021-01-07 09:55:57 +0100

    [MIG] Migration to v13

--- rgb_base_l10n_es/__manifest__.py
+++ rgb_base_l10n_es/__manifest__.py
@@ -1,23 +1,23 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 RGB Consulting SL
+# Copyright 2021 RGB Consulting SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'RGB Base Spanish Localization',
     'summary': 'Base Spanish localization modules',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'RGB',
     'website': "https://www.rgbconsulting.com",
     'author': "RGB Consulting SL",
     'license': 'AGPL-3',
-    'depends': ['rgb_base_account',
-                'l10n_es',
-                #10n-spain
-                'l10n_es_toponyms',
-                'l10n_es_partner',
-                'l10n_es_mis_report',
-                'l10n_es_account_invoice_sequence',
-                'l10n_es_partner_mercantil',
-                'l10n_es_account_bank_statement_import_n43'
+    'depends': [
+                    'rgb_base_account',
+                    'l10n_es',
+                # l10n-spain
+                    'l10n_es_toponyms',
+                    'l10n_es_partner',
+                    'l10n_es_mis_report',
+                    # 'l10n_es_account_invoice_sequence',
+                    'l10n_es_partner_mercantil',
+                    'l10n_es_account_bank_statement_import_n43'
     ],
     'description': """
 RGB Base Spanish Localization
