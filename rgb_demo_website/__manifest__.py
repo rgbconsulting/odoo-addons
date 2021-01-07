4e42bc75de561f8b28c661a198593846bd622930
Aleix <aleix.capdevila@rgbconsulting.com>
2021-01-07 10:02:18 +0100

    [MIG] Migrated to v13

--- rgb_demo_website/__manifest__.py
+++ rgb_demo_website/__manifest__.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 RGB Consulting SL
+# Copyright 2021 RGB Consulting
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
     'name': 'RGB Demo Website Installation',
     'summary': 'Default addons for demo website',
-    'version': '12.0.0.1.0',
+    'version': '13.0.1.0.0',
     'category': 'RGB',
     'website': "https://www.rgbconsulting.com",
     'author': "RGB Consulting SL",
@@ -17,9 +17,9 @@
                 'website_blog',
                 'website_forum',
                 'website_event',
-                'website_hr',
+                # 'website_hr', DEL
                 'website_hr_recruitment',
-    ],
+                ],
     'description': """
 RGB Demo Website Installation
 =============================
