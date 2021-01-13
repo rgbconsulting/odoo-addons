44f93119d0d156388713c8332409f688b007afd4
Ramon <ramon.garcia@rgbconsulting.com>
2021-01-13 16:19:11 +0100

    [ADD] hr_resources: new module added.

---
+++ hr_resources/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 RGB Consulting
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'HR Resources',
+    'summary': 'HR Resources',
+    'version': '13.0.1.0.0',
+    'category': 'Human Resources',
+    'website': 'https://www.rgbconsulting.com',
+    'author': 'RGB Consulting',
+    'license': 'AGPL-3',
+    'depends': ['hr'],
+    'description': """
+HR Resources
+============
+* This module adds the ressources menu from technical settings to employee configuration.
+    """,
+
+    'data': [
+        'views/hr_views.xml',
+    ],
+}
