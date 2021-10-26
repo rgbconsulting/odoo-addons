941a905cdd19eb332d58034d25aa0d85eaabfc63
RGB Informàtica i Consulting S.L <admin-rgbconsulting@users.noreply.github.com>
2021-10-26 13:21:56 +0200

    [T21-25883]

--- hr_my_attendance/__manifest__.py
+++ hr_my_attendance/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': "Hr My Attendace",
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'depends': ['hr_attendance'],
     'license': 'AGPL-3',
     'author': "RGB Consulting SL",
