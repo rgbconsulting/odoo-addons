28688523a127ac7cd826b2442e5414614907adc3
Nozarashi <luiscarlos.gonzalez@rgbconsulting.com>
2022-08-31 14:05:23 +0200

    [ADD] Mail activity subscribe [ADD] Field VAT transaltion fix

---
+++ mail_activity_subscribe/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 RGB Consulting SL
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': "Mail activity subcribe",
+    'version': '12.1.1.0.0',
+    'depends': ['mail'],
+    'license': 'AGPL-3',
+    'author': "RGB Consulting SL",
+    'website': "https://www.rgbconsulting.com",
+    'category': 'Discuss',
+    'summary': """Add user as activities follower """,
+    'description': """When a user creates a new activity is added as activities follower """,
+    'data': [],
+}
