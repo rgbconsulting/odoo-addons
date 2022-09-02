17c8b321435bf06d457daa3259f4e479cbd5a2df
Nozarashi <luiscarlos.gonzalez@rgbconsulting.com>
2022-09-02 08:41:59 +0200

    [ADD] Module auth_user_passkey migration

---
+++ auth_user_passkey/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 RGB Consulting SL
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Auth User Passkey',
+    'summary': 'Allow login with any user using another user password',
+    'version': '15.0.1.0.0',
+    'category': 'base',
+    'website': 'https://www.rgbconsulting.com',
+    'author': 'RGB Informàtica i Consulting SL',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'description': """
+Auth Passkey
+============
+*   Allows to login with any user using the password of one user. The user allowed
+    to login with any other user can be configured in the system parameters.
+    """,
+
+    'data': [
+        'data/system_param.xml',
+    ],
+}
