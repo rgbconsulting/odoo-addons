3e7545a646b924735c23064492f2c2735f94eedf
Nozarashi <luiscarlos.gonzalez@rgbconsulting.com>
2022-09-03 08:50:40 +0200

    [ADD] Module auth_user_passkey migration

--- auth_user_passkey/models/res_users.py
+++ auth_user_passkey/models/res_users.py
@@ -2,7 +2,7 @@
 # Copyright 2022 RGB Consulting SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import exceptions, models, api
+from odoo import SUPERUSER_ID, exceptions, models, api
 
 
 class ResUsers(models.Model):
@@ -11,7 +11,6 @@ class ResUsers(models.Model):
     @api.model
     def _check_credentials(self, password):
         user_id = int(self.env['ir.config_parameter'].with_user(SUPERUSER_ID).get_param('auth_user_passkey.user_id'))
-        
         if self._uid != user_id:
             try:
                 super(ResUsers, self)._check_credentials(password)
