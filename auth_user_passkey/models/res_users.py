1e106c3441182e54cd813eed098ae3924e4ae147
Nozarashi <luiscarlos.gonzalez@rgbconsulting.com>
2022-09-03 08:44:32 +0200

    [ADD] Module auth_user_passkey migration

--- auth_user_passkey/models/res_users.py
+++ auth_user_passkey/models/res_users.py
@@ -10,12 +10,13 @@ class ResUsers(models.Model):
 
     @api.model
     def _check_credentials(self, password):
-        user_id = int(self.env['ir.config_parameter'].sudo().get_param('auth_user_passkey.user_id'))
+        user_id = int(self.env['ir.config_parameter'].with_user(SUPERUSER_ID).get_param('auth_user_passkey.user_id'))
+        
         if self._uid != user_id:
             try:
                 super(ResUsers, self)._check_credentials(password)
                 return True
             except exceptions.AccessDenied:
-                return self.sudo(user_id)._check_credentials(password)
+                return self.with_user(user_id)._check_credentials(password)
         else:
             return super(ResUsers, self)._check_credentials(password)
