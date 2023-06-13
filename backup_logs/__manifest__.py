09f5def8ff93ec38551f80506a414eeadc53b41f
Luis Carlos González <luiscarlos.gonzalez@rgbconsulting.com>
2023-06-13 08:59:37 +0200

    [ADD] Backup logs module

--- backup_logs/__manifest__.py
+++ backup_logs/__manifest__.py
@@ -2,27 +2,27 @@
 # Copyright (C) 2023-Today RGB Informàtica i Consulting SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
 {
-    'name': "Login logs",
+    'name': "Backup logs",
     'version': '0.1',
     'category': 'Tools',
     'summary': """
-        Gets de journalctl login logs for the current instance
+        Gets backup logs for the current instance
         """,
     'author': 'RGB Informàtica i Consulting SL',
     'website': "https://www.rgbconsulting.com",
     'license': 'AGPL-3',
-
     'depends': ['base'],
 
     'description': """
-        Gets de journalctl login logs for the current instance
+        Gets backup logs for the current instance
     """,
 
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
         "data/ir_cron.xml",
-        'views/login_logs_views.xml',
+        'views/backup_log_type_views.xml',
+        'views/backup_log_record_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
