aee345eb63f5774f398fccf9d3d2882ad508ee1f
Luis Carlos González <luiscarlosgonzalezbarcenas@gmail.com>
2023-12-19 10:12:57 +0100

    [ADD] Import module vat_check_disable from previous version

---
+++ vat_check_disable/models/res_partner.py
@@ -0,0 +1,30 @@
+# Copyright 2022 RGB Consulting
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    vat_validation_fail = fields.Boolean(string='VAT Validation Fail', readonly=True, default=False)
+
+    @api.constrains('vat')
+    def check_vat(self):
+        if self.env.context.get('company_id'):
+            company = self.env['res.company'].browse(self.env.context['company_id'])
+        else:
+            company = self.env.user.company_id
+        if company.disable_vat_check:
+            for partner in self:
+                try:
+                    super(ResPartner, partner).check_vat()
+                    if partner.vat_validation_fail:
+                        partner.write({'vat_validation_fail': False})
+                except Exception as ex:
+                    if not partner.vat_validation_fail:
+                        partner.write({'vat_validation_fail': True})
+        else:
+            super(ResPartner, self).check_vat()
+            if any(rec.vat_validation_fail for rec in self):
+                self.write({'vat_validation_fail': False})
